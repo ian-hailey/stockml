d1fb90c494b11c3c5d98adf699e266524cf8d118
Ian Hailey <ian@hailey.dk>
2018-09-06 18:42:28 +0100

    total=64.52% (gains=64.52% losses=0.00%)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def plot_signal_lines(ax, signals, line_width):
             plt.axvline(x=signal.Index, linewidth=line_width, color='grey')
             plt.axvline(x=signal.t_out, linewidth=line_width, ls='--', color='grey')
 
-def plot_day(df_day_s, day, stop=10, signals=None):
+def plot_day(df_day_s, day, stop=10, signals=None, plotMA5s=False):
     # create date time index for the pre-market
     dates_pre = pd.date_range(day + pd.DateOffset(hours=4), day + pd.DateOffset(hours=9.5), freq='S')
     df_pre = df.daterange(dates_pre)
@@ -29,8 +29,8 @@ def plot_day(df_day_s, day, stop=10, signals=None):
         df_day_m = df_day_s.resample('60s')
         df_day_m.compute_rsi()
         df_day_m.compute_stoch()
-        df_day_m.compute_ma(time=5, source='Open')
-        df_day_m.compute_ma(time=10, source='Open')
+        df_day_m.compute_ma(time=5, source='Close')
+        df_day_m.compute_ma(time=10, source='Close')
 
         # create date time index for the first 30 minutes of market second resolution
         dates_open_s = pd.date_range(day + pd.DateOffset(hours=9.5), day + pd.DateOffset(hours=stop), freq='S')
@@ -65,10 +65,11 @@ def plot_day(df_day_s, day, stop=10, signals=None):
         df_open_m.data['Close'].plot(ax=ax, x_compat=True, color='black', label='SPm')
 
         # Plot the MA
-        df_open_s.data['ma5'] = df_open_s.data['ma5'] / df_open_s.data['Open'][0]
-        df_open_s.data['ma5'].plot(ax=ax, x_compat=True, color='purple', label='MA(5s)')
-        df_open_s.data['ma10'] = df_open_s.data['ma10'] / df_open_s.data['Open'][0]
-        df_open_s.data['ma10'].plot(ax=ax, x_compat=True, color='purple', ls='--', label='MA(10s)')
+        if plotMA5s:
+            df_open_s.data['ma5'] = df_open_s.data['ma5'] / df_open_s.data['Open'][0]
+            df_open_s.data['ma5'].plot(ax=ax, x_compat=True, color='purple', label='MA(5s)')
+            df_open_s.data['ma10'] = df_open_s.data['ma10'] / df_open_s.data['Open'][0]
+            df_open_s.data['ma10'].plot(ax=ax, x_compat=True, color='purple', ls='--', label='MA(10s)')
 
         df_open_m.data['ma5'] = df_open_m.data['ma5'] / df_open_m.data['Open'][0]
         df_open_m.data['ma5'].plot(ax=ax, x_compat=True, color='blue', label='MA(5)')
@@ -127,8 +128,8 @@ def find_signals(df_day_s, day, stop=10, resolution='M'):
         df_day_m = df_day_s.resample('60s')
         df_day_m.compute_rsi()
         df_day_m.compute_stoch()
-        df_day_m.compute_ma(time=5, source='Open')
-        df_day_m.compute_ma(time=10, source='Open')
+        df_day_m.compute_ma(time=5, source='Close')
+        df_day_m.compute_ma(time=10, source='Close')
 
         # create down samples minute resolution first 30 minutes
         dates_open_s = pd.date_range(day + pd.DateOffset(hours=9.5), day + pd.DateOffset(hours=stop), freq='S')
@@ -155,7 +156,7 @@ def find_signals(df_day_s, day, stop=10, resolution='M'):
             maDelta = (row.ma5 / open) - (row.ma10 / open)
  #           print("{} maIn={:.2f} rsiIn={:.0f} stochIn={:.0f} wick={:.4f} SP={:.2f}".format(row.Index.strftime('%Y-%m-%d %H:%M'),
  #                                                                               maDelta, row.rsi, row.stoch_k, wick, row.Close))
-            if spIn == 0 and row.Close > row.Open and row_m.rsi > 70 and row.stoch_k > 70 and maDelta > 0.001:
+            if spIn == 0 and row.Close > row.Open and row_m.rsi > 60 and row.stoch_k > 70 and maDelta > 0.001:
                 spIn = row.Close
                 stochIn = row.stoch_k
                 rsiIn = row_m.rsi
