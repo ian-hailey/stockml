18438722bba335a1977d15dcd02916b2018f0a95
Ian Hailey <ian@hailey.dk>
2018-09-27 19:57:42 +0100

    adding y to dataset

--- datasets.py
+++ datasets.py
@@ -112,7 +112,7 @@ class dataset(object):
             self.day = self.days[self.day_index]
         if self.day is not None:
             dates_day_s = pd.date_range(self.day + pd.DateOffset(hours=start), self.day + pd.DateOffset(hours=stop), freq='S')
-            self.data_day_s = self.data.daterange(dates_day_s)
+            self.data_day_s = self.data.daterange(dates_day_s, ohlcvOnly=False)
             self.data_day_m = self.data_day_s.resample(period='60s')
             dates_day = pd.date_range(self.day - pd.DateOffset(days=240), self.day - pd.DateOffset(days=1), freq='1D')
             self.data_day_d = self.data_d.daterange(dates_day)
@@ -123,17 +123,31 @@ class dataset(object):
 
     def reset_sec_index(self, time='09:30:00'):
         self.sec_index = self.data_day_s.data.index.get_loc(self.day.strftime('%Y-%m-%d') + ' ' + time)
+        self.sec_offset = self.sec_index
 
     def get_next_second(self):
-        minute_index = int(self.sec_index / 60)
-        # add last 240 days
-        state = self.data_d.data.values[self.day_index:self.day_index+240, :]
-        # add last 60 minutes
-        state = np.concatenate((state, self.data_day_m.data.values[minute_index - 60:minute_index, :]), axis=0)
-        # add last 60 seconds
-        state = np.concatenate((state, self.data_day_s.data.values[self.sec_index-60:self.sec_index,:]), axis=0)
+        x_state = None
+        y_state = self.data_day_s.data.values[self.sec_index][5]
+        if np.isnan(y_state) == False:
+            minute_index = int(self.sec_index / 60)
+            # add last 240 days
+            x_state = self.data_d.data.values[self.day_index:self.day_index+240, :]
+            # add last 240 minutes
+            x_state = np.concatenate((x_state, self.data_day_m.data.values[minute_index - 240:minute_index, :]), axis=0)
+            # add last 60 seconds
+            x_state = np.concatenate((x_state, self.data_day_s.data.values[self.sec_index-59:self.sec_index+1,:5]), axis=0)
+            open = self.data_day_s.data.values[self.sec_index][0]
+            x_state[:,:4] = x_state[:,:4] / open
+            # add time stamp planes
+            tmonth = np.full((x_state.shape[0], 1), int(self.day.strftime('%m')))
+            tday = np.full((x_state.shape[0], 1), int(self.day.strftime('%d')))
+            tsec = np.full((x_state.shape[0], 1), self.sec_index - self.sec_offset)
+            x_state = np.append(x_state, tmonth, 1)
+            x_state = np.append(x_state, tday, 1)
+            x_state = np.append(x_state, tsec, 1)
+            open = self.data_day_s.data.values[self.sec_index][0]
         self.sec_index = self.sec_index + 1
-        return state
+        return x_state, y_state
 
 def save_plots(data):
     fig = plt.figure(frameon=False, figsize=(8, 4), dpi=100)
@@ -142,12 +156,13 @@ def save_plots(data):
     ax.set_xlim(0, 360)
 
     for m in range(3600):
-        state = data.get_next_second()
-        ax.set_xlim(0, 360)
-        plt.plot(state[:,1]/state[120][3])
-        plt.plot(state[:,2]/state[120][3])
-        plt.plot(state[:,3]/state[120][3])
+        x_state, y_state = data.get_next_second()
+        ax.set_xlim(0, x_state.shape[0])
+        plt.plot(x_state[:,1])
+        plt.plot(x_state[:,2])
+        plt.plot(x_state[:,3])
 #        plot_ohlcv(ax, state)
+        plt.figtext(0.1, 0, "Y={}".format(y_state))
         fig.tight_layout()
         plt.autoscale(tight=True)
         filename = "data/wdc_{}_{}.jpg".format(data.day.strftime('%Y-%m-%d'), m)
@@ -156,8 +171,15 @@ def save_plots(data):
 
 print("Using:", matplotlib.get_backend())
 
+# load OHLCV
 df = data.ohlcv_csv("../ib/wdc_ohlcv_1_year.csv")
 df.fill_gaps()
+
+# load buysell data
+buysell = pd.read_csv("wdc_ohlcv_1_year_buysell.csv", header=0, index_col=0, parse_dates=True, infer_datetime_format=True)
+
+# merge the two
+df.data = df.data.join(buysell)
 
 print("loaded data types:\n" + str(df.data.dtypes))
 print(df.data.index)
@@ -169,15 +191,16 @@ print(data.get_date_range())
 error = data.select_day('2018-04-25')
 
 start_time = time.time()
-save_plots(data)
+#save_plots(data)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-#start_time = time.time()
-#for m in range(23400):
-#    state = data.get_next_second()
-#    state3d = state.reshape(6, 60, 5)
-#    print(state3d.shape)
-#print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+for m in range(23400):
+    x_state, y_state = data.get_next_second()
+    if x_state is not None:
+        x_state3d = x_state.reshape(9, 60, 8)
+        print("x_state3d={} y_state={}".format(x_state3d.shape, y_state))
+print("--- %s seconds ---" % (time.time() - start_time))
 
 #    data.plot_day_2d()
 #    state3d = state.reshape(6, 60, 5)
