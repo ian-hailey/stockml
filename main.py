2e669fe9b1a20085395557d0ae231e7bd06babd0
Ian Hailey <ian@hailey.dk>
2018-09-27 18:57:58 +0100

    speed stuff up

--- main.py
+++ main.py
@@ -125,6 +125,7 @@ def generate_buysell_signal(df_day_s, day, stop=10, resolution='M'):
         df_day_s.compute_ma(time=5, sample=60)
         df_day_s.compute_ma(time=10, sample=60)
         df_day_m = df_day_s.resample('60s')
+        # be careful not to change the order of these otherwise the row index mapping will change
         df_day_m.compute_ma(time=5, source='Close')
         df_day_m.compute_ma(time=10, source='Close')
 
@@ -133,30 +134,35 @@ def generate_buysell_signal(df_day_s, day, stop=10, resolution='M'):
         df_open_s = df_day_s.daterange(dates_open_s, ohlcvOnly=False)
         dates_open_m = pd.date_range(day + pd.DateOffset(hours=4.0), day + pd.DateOffset(hours=stop), freq='60S')
         df_open_m = df_day_m.daterange(dates_open_m, ohlcvOnly=False)
+        open = df_open_s.data['Open'][day.strftime('%Y-%m-%d') + ' 09:30:00']
+        df_open_m.data['Close'] = df_open_m.data['Close'] / open
+        df_open_m.data['ma5'] = df_open_m.data['ma5'] / open
+        df_open_m.compute_gradient(source='ma5')
+        zero_crossings = np.where(np.diff(np.sign(df_open_m.data['ma5grad'].values)))[0]
         if resolution is 'S':
             df_open = df_open_s
+            zero_crossings = zero_crossings * 60
         else:
             df_open = df_open_m
-        open = df_open_s.data['Open'][day.strftime('%Y-%m-%d') + ' 09:30:00']
-        df_open.data['Close'] = df_open.data['Close'] / open
-        df_open.data['ma5'] = df_open.data['ma5'] / open
-        df_open.compute_gradient(source='ma5')
+        openIndex = df_open.data.index.get_loc(day.strftime('%Y-%m-%d') + ' 09:30:00')
         print("{} pre_close={} pre_vol={}".format(day.strftime('%Y-%m-%d'), df_pre_close, df_pre_vol))
-        buysell = pd.DataFrame(index=df_open.data.index, columns=['buysell'])
-        zero_crossings = np.where(np.diff(np.sign(df_open.data['ma5grad'].values)))[0]
+        buysell = pd.DataFrame(0.0, index=df_open.data.index, columns=['buysell'])
+        nearest_zc = -1
+        buysell_values = buysell['buysell'].values
         for rowIndex in range(df_open.data.index.size):
-            row = df_open.data.iloc[rowIndex]
-#            print("row={} maDelta={}".format(rowIndex, row.ma5grad))
-            nearest_zc = min(zero_crossings, key=lambda x:x<rowIndex)
+            if nearest_zc < rowIndex:
+                nearest_zc = min(zero_crossings, key=lambda x: x < rowIndex)
+                nearest_zc_ma5 = df_open.data.iloc[nearest_zc].ma5
+            row = df_open.data.values[rowIndex] # not using iloc object as slow
+#            print("row={} nearest_zc={} maDelta={}".format(rowIndex, nearest_zc, row.ma5grad))
             if nearest_zc >= rowIndex:
-                buysell['buysell'].iloc[rowIndex] = df_open.data.iloc[nearest_zc].ma5 - row.ma5
-            else:
-                buysell['buysell'].iloc[rowIndex] = 0
-#        plot_buysell_signal(df_open.data['Close'].values, df_open.data['ma5'].values, buysell['buysell'].values)
+                buysell_values[rowIndex] = nearest_zc_ma5 - row[5]
+        plot_buysell_signal(day, df_open.data['Close'].values, df_open.data['ma5'].values, buysell['buysell'].values, openIndex)
+        buysell['buysell'].fillna(value=0.0, inplace=True)
         return buysell
     return None
 
-def plot_buysell_signal(close, ma5, buysell):
+def plot_buysell_signal(day, close, ma5, buysell, openIndex):
     fig = plt.figure(frameon=False, figsize=(8, 4), dpi=100)
     ax = fig.add_subplot(111)
     ax.plot(close)
@@ -164,16 +170,16 @@ def plot_buysell_signal(close, ma5, buysell):
     ax2 = ax.twinx()
     ax2.plot(buysell, color='green')
     plt.axhline(y=0.0, ls='--', color='grey')
-    plt.axvline(x=330, ls='--', color='grey')
+    plt.axvline(x=openIndex, ls='--', color='grey')
+    filename = "wdc_buysell/wdc_{}.jpg".format(day.strftime('%Y-%m-%d'))
+    fig.savefig(filename)
+    plt.close()
 
 buysell = pd.DataFrame(columns=['buysell'])
-#buysell_lock = Lock()
 
 def buysell_signal_callback(results):
     global buysell
-#    buysell_lock.acquire()
     buysell = pd.concat([buysell, results])
-#    buysell_lock.release()
 
 def buysell_from_df(df, days, stop=10, thread_count=1):
     global buysell
@@ -183,7 +189,7 @@ def buysell_from_df(df, days, stop=10, thread_count=1):
             for i in range(days.size):
                 dates_day_s = pd.date_range(days[i] + pd.DateOffset(hours=4), days[i] + pd.DateOffset(hours=stop), freq='S')
                 df_day_s = df.daterange(dates_day_s)
-                pool.apply_async(generate_buysell_signal, args=(df_day_s, days[i], stop, 'M'), callback=buysell_signal_callback)
+                pool.apply_async(generate_buysell_signal, args=(df_day_s, days[i], stop, 'S'), callback=buysell_signal_callback)
             pool.close()
             pool.join()
     else:
@@ -360,7 +366,7 @@ plt.close()
 days = pd.date_range(df.data.index[0], df.data.index[-1], freq='1D')
 days = days.normalize()
 
-threads = 6
+threads = 1
 buysell_from_df(df, days, stop=16, thread_count=threads)
 buysell.to_csv("wdc_ohlcv_1_year_buysell.csv")
 #signals_from_df(df, days, stop=16, thread_count=threads)
